3c3aa5d36a2d4935fee981299299e2b5eb8a94ef
KirillMar <re4r@bk.ru>
2023-03-10 16:27:40 +0300

    lesson3

--- src/main/java/lesson3/ArithmeticAndLogigalOperations.java
+++ src/main/java/lesson3/ArithmeticAndLogigalOperations.java
@@ -4,6 +4,31 @@ package lesson3;
 
 public class ArithmeticAndLogigalOperations {
     public static void main(String[] args) {
+        byte b = 0;
+        for (int i = 0; i < 10; i++) {
+            b += i;
+        }
+        System.out.println(b);
+        
+        short s = 100;
+        for (int i = 0; i < 10; i++) {
+            s -= i;
+        }
+        System.out.println(s);
+        
+        int i = 100;
+        for (int j = 1; j < 10; j++) {
+            i *= j;
+        }
+        System.out.println(i);
+        
+        long l = 1_000_000;
+        for (int j = 1; j < 10; j++) {
+            l /= j;
+        }
+        System.out.println(l);
+        
+        
         
     }
 }
