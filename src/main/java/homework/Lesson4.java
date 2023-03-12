20559ec6db2b3cb89ceed832242923e152276c2e
KirillMar <re4r@bk.ru>
2023-03-12 16:33:48 +0300

    lesson4_homework

--- src/main/java/homework/Lesson4.java
+++ src/main/java/homework/Lesson4.java
@@ -5,6 +5,8 @@ public class Lesson4 {
     
     public static void main(String[] args) {
         
+        Student student1 = new Student(10, "John", "Smith", 4.5, 4.7);
+        
     }
     
     class Student {
@@ -20,6 +22,9 @@ public class Lesson4 {
             this.studentPassNumber = spn;
             this.name = n;
             this.surname = sn;
+            this.yearOfStudying = yos;
+            this.economyAverage = ea;
+            this.foreignLanguageAverage = fla;
             
         }
     }
