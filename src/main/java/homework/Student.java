c1b0f98c9c2d93916c4e138a1013a4c873177f56
KirillMar <re4r@bk.ru>
2023-03-12 17:30:14 +0300

    lesson4_homework

--- src/main/java/homework/Student.java
+++ src/main/java/homework/Student.java
@@ -22,19 +22,19 @@ public class Student {
             
         }
     
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         
-        Student student1 = new Student(10, "John", "Smith", 2002, 4.5, 4.7, 3.8);
-        Student student2 = new Student(11, "Alex", "Veron", 2003, 3.8, 3.5, 4.2);
-        Student student3 = new Student(12, "Mike", "Tooet", 2004, 4.2, 3.4, 3.9);
-        
-        double average1 = (student1.economyAverage + student1.foreignLanguageAverage + student1.mathematics) / 3;
-        double average2 = (student2.economyAverage + student2.foreignLanguageAverage + student2.mathematics) / 3;
-        double average3 = (student3.economyAverage + student3.foreignLanguageAverage + student3.mathematics) / 3;
-        
-        System.out.println("Average: " + average1);
-        System.out.println("Average: " + average2);
-        System.out.println("Average: " + average3);
+            Student student1 = new Student(10, "John", "Smith", 2002, 4.5, 4.7, 3.8);
+            Student student2 = new Student(11, "Alex", "Veron", 2003, 3.8, 3.5, 4.2);
+            Student student3 = new Student(12, "Mike", "Tooet", 2004, 4.2, 3.4, 3.9);
+
+            double average1 = (student1.economyAverage + student1.foreignLanguageAverage + student1.mathematics) / 3;
+            double average2 = (student2.economyAverage + student2.foreignLanguageAverage + student2.mathematics) / 3;
+            double average3 = (student3.economyAverage + student3.foreignLanguageAverage + student3.mathematics) / 3;
+
+            System.out.println("Average: " + average1);
+            System.out.println("Average: " + average2);
+            System.out.println("Average: " + average3);
         
         
   
