9e02557b51a91a96051899b6a2fc2dc630a5ad90
KirillMar <re4r@bk.ru>
2023-03-12 19:52:48 +0300

    lesson5

--- src/main/java/lesson5/Lesson5.java
+++ src/main/java/lesson5/Lesson5.java
@@ -2,5 +2,22 @@
 package lesson5;
 
 public class Lesson5 {
+        
+        public static void main(String[] args) {
+                
+                Summator summator = new Summator();
+                int result = summator.sum(4, 4, 4);
+                System.out.println(result);
+                
+        }
     
+   
 }
+
+class Summator {
+        
+        int sum(int x, int y, int z) {
+                return x + y + z;
+        }
+}
+
