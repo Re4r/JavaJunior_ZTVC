88e6d3f5baa912a58ecae3063a5f40c087bbec7c
KirillMar <re4r@bk.ru>
2023-03-12 15:38:40 +0300

    lesson4

--- src/main/java/lesson4/Car.java
+++ src/main/java/lesson4/Car.java
@@ -6,10 +6,12 @@ public class Car {
     String color = "Red";
     String engine = "V8";
     
-class CarTest {
-
-        Car car1 = new Car(); 
+    public static void main(String[] args) {
+        
+        Car car1 = new Car();
         Car car2 = new Car();
-}
-    
+        
+        System.out.println(car1);
+        System.out.println(car2);
+    }
 }
