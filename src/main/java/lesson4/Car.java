5440b65635c18f21ca701670154c8794d49f7c91
KirillMar <re4r@bk.ru>
2023-03-12 15:34:47 +0300

    lesson4

---
+++ src/main/java/lesson4/Car.java
@@ -0,0 +1,15 @@
+
+package lesson4;
+
+public class Car {
+    
+    String color = "Red";
+    String engine = "V8";
+    
+class CarTest {
+
+        Car car1 = new Car(); 
+        Car car2 = new Car();
+}
+    
+}
