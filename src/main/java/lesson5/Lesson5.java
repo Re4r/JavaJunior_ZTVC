9176a9db7c179f52c12413721eaefec7ec9e067f
KirillMar <re4r@bk.ru>
2023-03-12 19:35:48 +0300

    lesson5

--- src/main/java/lesson5/Lesson5.java
+++ src/main/java/lesson5/Lesson5.java
@@ -3,17 +3,4 @@ package lesson5;
 
 public class Lesson5 {
     
-    int sum(int x, int y, int z) {
-        return x + y + z;
-    }
-    
-class Test {
-    
-    public static void main(String[] args) {
-        
-        Lesson5 obj5 = new Lesson5();
-        obj5.sum(4, 4, 4);
-    }
-}
-
 }
