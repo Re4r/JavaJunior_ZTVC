c65e8389c6170aaefe1a6d15eec865dc5e73f737
KirillMar <re4r@bk.ru>
2023-03-12 14:47:37 +0300

    lesson4

---
+++ src/main/java/lesson4/BankAccount.java
@@ -0,0 +1,17 @@
+
+package lesson4;
+
+
+public class BankAccount {
+    public static void main(String[] args) {
+        
+        int id;
+        String name;
+        double balance;
+        
+        BankAccount bankAccount = new BankAccount();
+        
+        System.out.println(bankAccount);
+       
+    }
+}
