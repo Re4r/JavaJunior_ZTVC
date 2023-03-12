b5529e3b62f8ac046f07e838ba0184635029c41b
KirillMar <re4r@bk.ru>
2023-03-12 16:12:11 +0300

    lesson4_homework

---
+++ src/main/java/homework/Lesson4.java
@@ -0,0 +1,27 @@
+
+package homework;
+
+public class Lesson4 {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+    class Student {
+        
+        int studentPassNumber;
+        String name;
+        String surname;
+        int yearOfStudying;
+        double economyAverage;
+        double foreignLanguageAverage;
+        
+        public Student(int spn, String n, String sn, int yos, double ea, double fla) {
+            this.studentPassNumber = spn;
+            this.name = n;
+            this.surname = sn;
+            
+        }
+    }
+
+}
