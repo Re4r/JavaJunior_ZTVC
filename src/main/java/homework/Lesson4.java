83abe8ecd033f4cb24aabd3b317c168395b6e136
KirillMar <re4r@bk.ru>
2023-03-12 16:46:23 +0300

    lesson4_homework

--- src/main/java/homework/Lesson4.java
+++ src/main/java/homework/Lesson4.java
@@ -5,7 +5,7 @@ public class Lesson4 {
     
     public static void main(String[] args) {
         
-        Student student1 = new Student(10, "John", "Smith", 4.5, 4.7);
+        System.out.println(student1);
         
     }
     
@@ -17,16 +17,24 @@ public class Lesson4 {
         int yearOfStudying;
         double economyAverage;
         double foreignLanguageAverage;
+        double mathematics;
         
-        public Student(int spn, String n, String sn, int yos, double ea, double fla) {
+        public Student(int spn, String n, String sn, int yos, double ea, double fla, double mat) {
             this.studentPassNumber = spn;
             this.name = n;
             this.surname = sn;
             this.yearOfStudying = yos;
             this.economyAverage = ea;
             this.foreignLanguageAverage = fla;
+            this.mathematics = mat;
             
         }
+        
+        Student student1 = new Student(10, "John", "Smith", 2002, 4.5, 4.7, 3.8);
+        Student student2 = new Student(11, "Alex", "Veron", 2003, 3.8, 3.5, 4.2);
+        Student student3 = new Student(12, "Mike", "Tooet", 2004, 4.2, 3.4, 3.9);
+        
+        
     }
 
 }
