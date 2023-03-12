4a5b3108cf304f46f394e00564bc6b9d637991b0
KirillMar <re4r@bk.ru>
2023-03-12 20:05:38 +0300

    lesson5

--- src/main/java/lesson5/Lesson5.java
+++ src/main/java/lesson5/Lesson5.java
@@ -6,9 +6,13 @@ public class Lesson5 {
         public static void main(String[] args) {
                 
                 Summator summator = new Summator();
-                int result = summator.sum(4, 4, 4);
+                int result = summator.sum(50, 60, 70);
                 System.out.println(result);
                 
+                Car car = new Car();
+                car.acc(result);
+                car.brake(result);
+                car.info();
         }
     
    
@@ -18,6 +22,25 @@ class Summator {
         
         int sum(int x, int y, int z) {
                 return x + y + z;
+        }
+}
+
+class Car {
+        
+        String color = "Orange";
+        String engine = "V8";
+        int speed;
+        
+        int acc(int x) {
+                return speed += x;
+        }
+        
+        int brake(int x) {
+                return speed -= x;
+        }
+        
+        void info() {
+                System.out.println(color + engine + speed);
         }
 }
 
