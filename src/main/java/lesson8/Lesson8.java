80f231c3752b32ae226d78077db43242058b040c
KirillMar <re4r@bk.ru>
2023-03-13 20:53:31 +0300

    lesson8

--- src/main/java/lesson8/Lesson8.java
+++ src/main/java/lesson8/Lesson8.java
@@ -2,9 +2,19 @@
 package lesson8;
 
 public class Lesson8 {
-        
+
         public static void main(String[] args) {
                 
+                Test test = new Test();
+                
+                System.out.println(test.A + test.B);
+
         }
 
+}
+
+class Test {
+        
+        public final int A = 10;
+        public final int B = 20;
 }
