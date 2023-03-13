d955366e123a572a2776cdeac011c5dcb75681b5
KirillMar <re4r@bk.ru>
2023-03-13 19:26:47 +0300

    lesson7

--- src/main/java/lesson7/B.java
+++ src/main/java/lesson7/B.java
@@ -2,5 +2,32 @@
 package lesson7;
 
 public class B {
+        
+        public int id;
+        private int year;
+        double salary;
+        protected int number;
+        
+        public B(int id) {
+                this.id = id;
+        }
+        
+        private B(int id, int year) {
+                this.id = id;
+                this.year = year;
+        }
+        
+        B(int id, int year, double salary) {
+                this.id = id;
+                this.year = year;
+                this.salary = salary;
+        }
+        
+        protected B(int id, int year, double salary, int number) {
+                this.id = id;
+                this.year = year;
+                this.salary = salary;
+                this.number = number;
+        }
 
 }
