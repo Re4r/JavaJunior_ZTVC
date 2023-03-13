668ec2309ee1848579ffff2f585e2ca3f5d791d8
KirillMar <re4r@bk.ru>
2023-03-13 21:02:25 +0300

    lesson8

--- src/main/java/lesson8/Lesson8.java
+++ src/main/java/lesson8/Lesson8.java
@@ -5,16 +5,19 @@ public class Lesson8 {
 
         public static void main(String[] args) {
                 
-                Test test = new Test();
+                Test test = new Test(100, 300);
                 
                 System.out.println(test.A + test.B);
-
         }
-
 }
 
 class Test {
         
-        public final int A = 10;
-        public final int B = 20;
+        public final int A;
+        public final int B;
+
+        public Test(int A, int B) {
+                this.A = A;
+                this.B = B;
+        }
 }
