f654524ca61142a442a06d80cf46e5252f2561de
KirillMar <re4r@bk.ru>
2023-03-13 21:42:11 +0300

    lesson8

--- src/main/java/lesson6/C.java
+++ src/main/java/lesson6/C.java
@@ -3,13 +3,14 @@ package lesson6;
 
 import lesson7.B;
 
-public class C extends B  {
+public class C extends  B  {
         
         public static void main(String[] args) {
                 C c = new C(100);
                 c.show();
                 B b = new B(100, 200, 200.8, 900);
                 b.show();
+                
         }
 
         public C(int id) {
