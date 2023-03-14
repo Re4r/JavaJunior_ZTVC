aaaa9bbdfc3c844d3c7a759ee26aa728f024d864
KirillMar <re4r@bk.ru>
2023-03-14 20:33:43 +0300

    lesson9

--- src/main/java/lesson9/Lesson9.java
+++ src/main/java/lesson9/Lesson9.java
@@ -13,6 +13,47 @@ class Car {
         private String color;
         private String engine;
         private int price;
+
+        public Car(String color, String engine, int price) {
+                this.color = color;
+                this.engine = engine;
+                this.price = price;
+                count++;
+        }
+
+        public String getColor() {
+                return color;
+        }
+
+        public String getEngine() {
+                return engine;
+        }
+
+        public int getPrice() {
+                return price;
+        }
+
+        public void setColor(String color) {
+                this.color = color;
+        }
+
+        public void setEngine(String engine) {
+                this.engine = engine;
+        }
+
+        public void setPrice(int price) {
+                this.price = price;
+        }
+        
+        
+        
+        public void showColor() {
+                System.out.println("Color: " + color);
+        }
+        
+        public void changePrice() {
+                
+        }
         
         
 } 
