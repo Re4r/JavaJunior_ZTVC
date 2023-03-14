009c10b82f643354aa9eb86249114c69c5ee55aa
KirillMar <re4r@bk.ru>
2023-03-14 12:19:02 +0300

    lesson8

--- src/main/java/lesson8/Lesson8_1.java
+++ src/main/java/lesson8/Lesson8_1.java
@@ -4,6 +4,14 @@ package lesson8;
 public class Lesson8_1 {
         public static void main(String[] args) {
                 
+                Student student1 = new Student("Alex", 1);
+                Student student2 = new Student("Vira", 2);
+                Student student3 = new Student("Mike", 3);
+                
+                student1.showInfo();
+                student2.showInfo();
+                student3.showInfo();
+                
         }
 }
 
@@ -13,5 +21,15 @@ class Student {
         int course;
         static int count;
         
+        public Student(String name, int course) {
+                this.name = name;
+                this.course = course;
+                count++;
+                System.out.println("Student: " + count + " created");
+                
+        }
         
+        public void showInfo() {
+                System.out.println("Student: " + count + " created");
+        }
 }
