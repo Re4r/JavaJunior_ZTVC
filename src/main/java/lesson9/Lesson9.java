c05b735df86348e0cbe7ab1fa7292329122f3522
KirillMar <re4r@bk.ru>
2023-03-14 20:28:51 +0300

    lesson9

---
+++ src/main/java/lesson9/Lesson9.java
@@ -0,0 +1,18 @@
+
+package lesson9;
+
+public class Lesson9 {
+        public static void main(String[] args) {
+                
+        }
+}
+
+class Car {
+        
+        private static int count;
+        private String color;
+        private String engine;
+        private int price;
+        
+        
+} 
