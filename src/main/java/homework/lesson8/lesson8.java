11728eba8935c7d3eb690578670aa3c520074dfa
KirillMar <re4r@bk.ru>
2023-03-14 13:14:55 +0300

    lesson8_homework

--- src/main/java/homework/lesson8/lesson8.java
+++ src/main/java/homework/lesson8/lesson8.java
@@ -2,5 +2,19 @@
 package homework.lesson8;
 
 public class lesson8 {
+        public static void main(String[] args) {
+                
+                Test.division(Test.multiply(2, 4, 6), 5);
+        }
+}
 
+class Test {
+        
+        protected static int multiply(int a, int b, int c) {
+                return a * b * c;
+        }
+        
+        protected static void division(int x, int y) {
+                System.out.println(x / y);
+        }
 }
