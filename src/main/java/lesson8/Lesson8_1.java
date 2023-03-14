11cb9a37fc7baec3c24a66752fa2e65d573f4bd3
KirillMar <re4r@bk.ru>
2023-03-14 12:11:11 +0300

    lesson8

---
+++ src/main/java/lesson8/Lesson8_1.java
@@ -0,0 +1,17 @@
+
+package lesson8;
+
+public class Lesson8_1 {
+        public static void main(String[] args) {
+                
+        }
+}
+
+class Student {
+        
+        String name;
+        int course;
+        static int count;
+        
+        
+}
