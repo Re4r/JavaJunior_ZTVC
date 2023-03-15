5b703665b9766f51fbfe22349f9b004422070491
KirillMar <re4r@bk.ru>
2023-03-15 14:24:25 +0300

    lesson10

--- src/main/java/lesson10/Lesson10.java
+++ src/main/java/lesson10/Lesson10.java
@@ -1,12 +1,15 @@
 
 package lesson10;
 
-
+import lesson9.Car;
+import lesson8.Tesv;
 
 public class Lesson10 {
         public static void main(String[] args) {
                 
-                lesson9.Car car = new lesson9.Car("Red", "V6", 3400);
+                Car car = new Car("Red", "V6", 3400);
                 car.showColor();
+                Tesv tesv = new Tesv(45, 78);
+                tesv.info();
         }
 }
