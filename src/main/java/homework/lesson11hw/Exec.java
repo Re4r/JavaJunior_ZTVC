046e5c61adc1246073bd5400e88dd697e5f93eb7
KirillMar <re4r@bk.ru>
2023-03-15 19:41:04 +0300

    lesson11_homework

--- src/main/java/homework/lesson11hw/Exec.java
+++ src/main/java/homework/lesson11hw/Exec.java
@@ -4,5 +4,13 @@ package homework.lesson11hw;
 public class Exec {
         public static void main(String[] args) {
                 
+                Car car1 = new Car("Black", "V8", 4);
+                Car car2 = new Car("Purple", "V6", 2);
+                
+                car1.changeNumbersOfDoors(8);
+                car2.changeNumbersOfDoors(16);
+                
+                System.out.println(car1.getNumberOfDoors());
+                System.out.println(car2.getNumberOfDoors());
         }
 }
