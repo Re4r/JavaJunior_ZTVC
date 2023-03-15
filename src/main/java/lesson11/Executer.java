d96584af7dd0a51c9c6b1b47ed6998706d53f059
KirillMar <re4r@bk.ru>
2023-03-15 19:04:37 +0300

    lesson11

--- src/main/java/lesson11/Executer.java
+++ src/main/java/lesson11/Executer.java
@@ -2,6 +2,7 @@
 package lesson11;
 
 public class Executer {
+        
         public static void main(String[] args) {
                 
                 Employee employee = new Employee("Trevor", 2000);
@@ -9,5 +10,8 @@ public class Executer {
                 System.out.println(employee.x2Salary());
                 employee.giveSalary();
                 System.out.println(employee.getSalary());
+                
+                Student student1 = new Student("Jannet", 3, 4.5);
+                Student student2 = new Student("Falcon", 4, 3.8);
         }
 }
