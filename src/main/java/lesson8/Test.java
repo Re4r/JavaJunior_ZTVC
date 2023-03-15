6ca3d91f83ea871db8aad48b9486f7ff5cb205be
KirillMar <re4r@bk.ru>
2023-03-15 14:20:44 +0300

    lesson10

---
+++ src/main/java/lesson8/Test.java
@@ -0,0 +1,19 @@
+
+package lesson8;
+
+public class Test {
+        
+        private final int A;
+        private final int B;
+
+        public Test(int A, int B) {
+                this.A = A;
+                this.B = B;
+        }
+        
+        public void info() {
+                System.out.println(this.A);
+                System.out.println(this.B);
+        }
+
+}
