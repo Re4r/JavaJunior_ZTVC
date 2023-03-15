7c12e268e1832592a2b059d5fcf87fe4e46e140f
KirillMar <re4r@bk.ru>
2023-03-15 14:07:08 +0300

    lesson10

--- src/main/java/lesson10/Lesson10.java
+++ src/main/java/lesson10/Lesson10.java
@@ -6,5 +6,6 @@ package lesson10;
 public class Lesson10 {
         public static void main(String[] args) {
                 
+                lesson9.Car car = new lesson9.Car("Red", "V6", 3400);
         }
 }
