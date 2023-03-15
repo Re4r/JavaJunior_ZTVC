001fd18f13b27ea06de3fa45ba5c9a256a0808c3
KirillMar <re4r@bk.ru>
2023-03-15 20:35:24 +0300

    lesson12

---
+++ src/main/java/lesson12/Car.java
@@ -0,0 +1,24 @@
+
+package lesson12;
+
+public class Car {
+        
+        private int engine;
+        private int doorCount;
+
+        public Car(int engine, int doorCount) {
+                this.engine = engine;
+                this.doorCount = doorCount;
+        }
+
+        public int getEngine() {
+                return engine;
+        }
+
+        public int getDoorCount() {
+                return doorCount;
+        }
+        
+        
+
+}
---
+++ src/main/java/lesson12/Exec.java
@@ -0,0 +1,16 @@
+
+package lesson12;
+
+public class Exec {
+        public static void main(String[] args) {
+                
+                Car car1 = new Car(8, 4);
+                Car car2 = new Car(6, 2);
+                
+                if (car1.getEngine() > car2.getEngine()) {
+                        if (car1.getDoorCount() > car2.getDoorCount()) {
+                                
+                        }
+                }
+        }
+}
