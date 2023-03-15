f36ec7c141515485164a1642f75e559d2e6907f5
KirillMar <re4r@bk.ru>
2023-03-15 20:58:40 +0300

    lesson12

--- src/main/java/lesson12/Exec.java
+++ src/main/java/lesson12/Exec.java
@@ -5,7 +5,7 @@ public class Exec {
         public static void main(String[] args) {
                 
                 Car car1 = new Car(8, 4);
-                Car car2 = new Car(6, 2);
+                Car car2 = new Car(6, 6);
                 
                 if (car1.getEngine() > car2.getEngine()) {
                         if (car1.getDoorCount() > car2.getDoorCount()) {
@@ -20,3 +20,4 @@ public class Exec {
                 System.out.println(Car.getGrade());
         }
 }
+
