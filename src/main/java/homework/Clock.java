c357fafc6a7acff9fa2383d3412b943a29f9e375
KirillMar <re4r@bk.ru>
2023-03-16 17:17:50 +0300

    lesson14_homework

--- src/main/java/homework/Clock.java
+++ src/main/java/homework/Clock.java
@@ -2,5 +2,25 @@
 package homework;
 
 public class Clock {
+        
+        public static void main(String[] args) {
+                time();
+        }
+        
+        static void time() {
+                OUTER: for (int i = 0; i < 6; i++) {
+                        INNER: for (int j = 0; j < 60; j++) {
+                                        for (int k = 0; k < 60; k++) {
+                                                System.out.println(i + ":" + j + ":" + k);
+
+                                                if (i > 1 && j % 10 == 0) {
+                                                        break OUTER;
+                                                } else if (k * i > j) {
+                                                        continue INNER;
+                                                }
+                                        }
+                        }
+                }
+        }
 
 }
