f9f149d21c2049256a5ea4783561c8c2d84d27ae
KirillMar <re4r@bk.ru>
2023-03-16 16:30:24 +0300

    lesson14

--- src/main/java/lesson14/Exec14.java
+++ src/main/java/lesson14/Exec14.java
@@ -6,7 +6,9 @@ public class Exec14 {
         public static void main(String[] args) {
                 
                 for (int i = 0; i < 100; i++) {
-                        if (i % 2 == 0) {
+                        if (i == 50) {
+                                break;
+                        } else if (i % 2 == 0) {
                                 System.out.println(i);
                         }
                 }
