2e6ed21be66d1864786ddaa5a2fc210703fea6fd
KirillMar <re4r@bk.ru>
2023-03-16 13:46:14 +0300

    lesson12_homework

--- src/main/java/homework/lesson12hw/Exec.java
+++ src/main/java/homework/lesson12hw/Exec.java
@@ -1,14 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
 package homework.lesson12hw;
 
-/**
- * 
- * @author Sammy Guergachi <sguergachi at gmail.com>
- */
 public class Exec {
-
+        public static void main(String[] args) {
+                
+        }
 }
--- src/main/java/homework/lesson12hw/Student.java
+++ src/main/java/homework/lesson12hw/Student.java
@@ -1,14 +1,52 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
 package homework.lesson12hw;
 
-/**
- * 
- * @author Sammy Guergachi <sguergachi at gmail.com>
- */
 public class Student {
+        
+        private String name;
+        private int course;
+        private double averageGrade;
+
+        public Student(String name, int course, double averageGrade) {
+                this.name = name;
+                this.course = course;
+                this.averageGrade = averageGrade;
+        }
+
+        public String getName() {
+                return name;
+        }
+
+        public int getCourse() {
+                return course;
+        }
+
+        public double getAverageGrade() {
+                return averageGrade;
+        }
+        
+        void equalStudents1(Student st1, Student st2) {
+                if (st1.equals(st2)) {
+                        System.out.println("Students are equal");
+                } else {
+                        System.out.println("Students are not equal");
+                }
+        }
+        
+        void equalStudents2 (Student st1, Student st2) {
+                if (st1.getName().equals(st2.getName())) {
+                        if (st1.getCourse() == st2.getCourse()) {
+                                if (st1.getAverageGrade() == st2.getAverageGrade()) {
+                                        System.out.println("Studets are fully equal");
+                                } else {
+                                        System.out.println("Average grade are differ");
+                                }
+                        } else {
+                                System.out.println("Course are differ");
+                        }
+                } else {
+                        System.out.println("Names are differ");
+                } 
+        }
 
 }
