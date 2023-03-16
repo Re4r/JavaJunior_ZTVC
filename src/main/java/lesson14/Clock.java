5e6b22060ee67687907176f3a23e6d7b0e393b72
KirillMar <re4r@bk.ru>
2023-03-16 16:52:59 +0300

    lesson14

--- src/main/java/lesson14/Clock.java
+++ src/main/java/lesson14/Clock.java
@@ -11,9 +11,11 @@ public class Clock {
         
         static void time() {
                 for (int i = 0; i < 24; i++) {
+                        System.out.println("start of outer loop...");
                         for (int j = 0; j < 60; j++) {
                                 System.out.println(i + " : " + j);
                         }
+                        System.out.println("end of outer loop");
                 }
         }
 }
