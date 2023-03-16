263c27927b3fc0ea41893456e8ad168dc25f3b3b
KirillMar <re4r@bk.ru>
2023-03-16 14:51:51 +0300

    lesson13_homework

--- src/main/java/homework/lesson13hw/Exec.java
+++ src/main/java/homework/lesson13hw/Exec.java
@@ -1,14 +1,57 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
 package homework.lesson13hw;
 
-/**
- * 
- * @author Sammy Guergachi <sguergachi at gmail.com>
- */
 public class Exec {
+        
+        public static void main(String[] args) {
+                
+                Exec.showDaysQuantity(Month.JUNE);
+                
+        }
+        
+        static void showDaysQuantity(Month month) {
+                switch (month) {
+                        case JANUARY:
+                                System.out.println("30");
+                                break;
+                        case FEBRUARY:
+                                System.out.println("31");
+                                break;
+                        case MARCH:
+                                System.out.println("134");
+                                break;
+                        case APRIL:
+                                System.out.println("456");
+                                break;
+                        case MAY:
+                                System.out.println("378");
+                                break;
+                        case JUNE:
+                                System.out.println("987");
+                                break;
+                        case JULY:
+                                System.out.println("145");
+                                break;
+                        case AUGUST:
+                                System.out.println("654");
+                                break;
+                        case SEPTEMBER:
+                                System.out.println("789");
+                                break;
+                        case OCTOBER:
+                                System.out.println("632");
+                                break;
+                        case NOVEMBER:
+                                System.out.println("907");
+                                break;
+                        case DECEMBER:
+                                System.out.println("342");
+                                break;
+                        default:
+                                throw new AssertionError();
+                }
+        }
+        
+                
 
 }
