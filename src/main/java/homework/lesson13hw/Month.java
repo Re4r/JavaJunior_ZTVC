0c24a04f99b64dd11d83e5d0459a84c90d9e481c
KirillMar <re4r@bk.ru>
2023-03-16 14:30:21 +0300

    lesson13_homework

---
+++ src/main/java/homework/lesson13hw/Month.java
@@ -0,0 +1,18 @@
+
+package homework.lesson13hw;
+
+public enum Month {
+        JANUARY,
+        FEBRUARY,
+        MARCH,
+        APRIL,
+        MAY,
+        JUNE,
+        JULY,
+        AUGUST,
+        SEPTEMBER,
+        OCTOBER,
+        NOVEMBER,
+        DECEMBER
+}
+
