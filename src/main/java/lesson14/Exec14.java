3ac2f71cb76ba1c723dfce5091eba77941655266
KirillMar <re4r@bk.ru>
2023-03-16 15:42:37 +0300

    lesson14

--- src/main/java/lesson14/Exec14.java
+++ src/main/java/lesson14/Exec14.java
@@ -5,6 +5,10 @@ public class Exec14 {
         
         public static void main(String[] args) {
                 
+                
+                for (int x = (int)(Math.random() * 10); x <= 10; x++) {
+                        System.out.println(x);
+                }
         }
 
 }
