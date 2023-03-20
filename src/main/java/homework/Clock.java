8a25cf29af328c95261e158f76828b8e00a78dd4
KirillMar <re4r@bk.ru>
2023-03-20 14:18:54 +0300

    lesson14

--- src/main/java/homework/Clock.java
+++ src/main/java/homework/Clock.java
@@ -12,7 +12,6 @@ public class Clock {
                         MIDDLE: for (int j = 0; j < 60; j++) {
                                 INNER: for (int k = 0; k < 60; k++) {
                                                 System.out.println(i + ":" + j + ":" + k);
-
                                                 if (i > 1 && j % 10 == 0) {
                                                         break OUTER;
                                                 } else if (k * i > j) {
