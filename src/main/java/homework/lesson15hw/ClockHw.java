075bb3329fb667e511dd7219a7703b050470b202
KirillMar <re4r@bk.ru>
2023-03-20 18:42:16 +0300

    lesson15_homework

---
+++ src/main/java/homework/lesson15hw/ClockHw.java
@@ -0,0 +1,33 @@
+
+package homework.lesson15hw;
+
+public class ClockHw {
+        
+        public static void main(String[] args) {
+                time();
+        }
+        
+        private static void time() {
+                int hour = 0;
+                OUTER: while (hour < 6) {
+                        int minute = -1;
+                        MIDDLE: do {
+                                minute++;
+                                if (hour > 1 && minute % 10 == 0) {
+                                        break OUTER;
+                                }
+                                int second = 0;
+                                INNER: while (second < 60) {
+                                        if (second * hour > minute) {
+                                                continue MIDDLE;
+                                        }
+                                        System.out.println(hour + ":" + minute + ":" + second);
+                                        second++;
+                                }
+                        } while (minute < 59);
+                        hour++;
+                }
+
+        }
+
+}
