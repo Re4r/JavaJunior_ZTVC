716c1a87ce775aeebe6e97ddf47d1c567b993ccd
KirillMar <re4r@bk.ru>
2023-03-20 15:15:42 +0300

    lesson15

--- src/main/java/lesson15/ClockWhileLoop.java
+++ src/main/java/lesson15/ClockWhileLoop.java
@@ -4,11 +4,19 @@ package lesson15;
 public class ClockWhileLoop {
         
         public static void main(String[] args) {
-                
+                time();
         }
         
-        private static void Time() {
-                
+        private static void time() {
+                int hour = 0;
+                OUTER: do {
+                        int minute = 0;
+                        INNER: while (minute < 60) {
+                                System.out.println(hour + ":" + minute);
+                                minute++;
+                        }
+                        hour++;
+                } while (hour < 24);
         }
 
 }
