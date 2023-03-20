c742c71edeedc685373e6b9f40de218c73050980
KirillMar <re4r@bk.ru>
2023-03-20 21:07:05 +0300

    lesson16

--- src/main/java/lesson16/Exec16_3.java
+++ src/main/java/lesson16/Exec16_3.java
@@ -2,5 +2,14 @@
 package lesson16;
 
 public class Exec16_3 {
+        
+        public static void main(String[] args) {
+                
+                String string = "  HelloMeteor  ";
+                String string1 = "TechnoRave";
+                
+                String string2 = string.trim().replace("Hello", string1.substring(0, 6));
+                System.out.println(string2);
+        }
 
 }
