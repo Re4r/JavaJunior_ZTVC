07a8de974c2ea367dee17ca1e0de6989c6e6e795
KirillMar <re4r@bk.ru>
2023-03-20 20:04:46 +0300

    lesson16

--- src/main/java/lesson16/Exec16.java
+++ src/main/java/lesson16/Exec16.java
@@ -6,8 +6,19 @@ public class Exec16 {
         public static void main(String[] args) {
                 
                 String string = new String("Hello");
+                String string1 = "Hello";
+                
                 int a = string.length();
+                int b = string1.length();
+                char c = string.charAt(b - 1);
+                int d = string1.indexOf('e');
+                int e = string.indexOf("llo");
+                
                 System.out.println(a);
+                System.out.println(b);
+                System.out.println(c);
+                System.out.println(d);
+                System.out.println(e);
                         
         }
 
