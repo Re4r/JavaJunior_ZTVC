88b11e7c847fb37125c3adee74d05f59a1a59c28
KirillMar <re4r@bk.ru>
2023-03-20 20:57:16 +0300

    lesson16

--- src/main/java/lesson16/Exec16_2.java
+++ src/main/java/lesson16/Exec16_2.java
@@ -13,6 +13,8 @@ public class Exec16_2 {
                 String s3 = string.replace("Hello", "GoodBye");
                 String s4 = string.concat(s3);
                 String s5 = s4.toLowerCase();
+                String s6 = s5.toUpperCase();
+                boolean b1 = s6.contains(s.toUpperCase());
                 
                 System.out.println(s);
                 System.out.println(s1);
@@ -20,6 +22,9 @@ public class Exec16_2 {
                 System.out.println(s3);
                 System.out.println(s4);
                 System.out.println(s5);
+                System.out.println(s6);
+                System.out.println(b1);
+                
         }
 
 }
