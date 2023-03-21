bb15a45c9bac069f36c1cddffcc22f1493518ae6
KirillMar <re4r@bk.ru>
2023-03-21 15:23:34 +0300

    lesson18

--- src/main/java/lesson18/Exec18.java
+++ src/main/java/lesson18/Exec18.java
@@ -5,6 +5,20 @@ public class Exec18 {
         
         public static void main(String[] args) {
                 
+                StringBuilder sb1 = new StringBuilder();
+                StringBuilder sb2 = new StringBuilder("HelloMeteor");
+                StringBuilder sb3 = new StringBuilder("Type");
+                StringBuilder sb4 = new StringBuilder(sb2);
+                
+                sb2.append(sb4);
+                sb2.insert(7, sb3);
+                
+                //System.out.println(sb4.subSequence(0, 7));
+                System.out.println(sb2);
+                
+                
+                
+               
         }
 
 }
