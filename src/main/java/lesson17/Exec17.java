c3c9ca10d8ffdb82d648d39ccb344de1f8d72c6d
KirillMar <re4r@bk.ru>
2023-03-21 14:28:47 +0300

    lesson16_homework

--- src/main/java/lesson17/Exec17.java
+++ src/main/java/lesson17/Exec17.java
@@ -4,8 +4,16 @@ package lesson17;
 public class Exec17 {
         
         public static void main(String[] args) {
-                String s1 = "Hello";
+                String s1 = "          Hello          ";
                 System.out.println(s1.isBlank());
+                String s2 = "         ";
+                System.out.println(s2.isBlank());
+                String s3 = "";
+                System.out.println(s3.isEmpty());
+                System.out.println(s1.strip());
+                System.out.println(s1.stripLeading());
+                System.out.println(s1.stripTrailing());
+                
         }
 
 }
