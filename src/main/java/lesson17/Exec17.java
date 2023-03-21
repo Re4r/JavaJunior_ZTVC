b365ad29de9cdddbff63fca3361e344cc218e7d7
KirillMar <re4r@bk.ru>
2023-03-21 14:22:26 +0300

    lesson16_homework

---
+++ src/main/java/lesson17/Exec17.java
@@ -0,0 +1,11 @@
+
+package lesson17;
+
+public class Exec17 {
+        
+        public static void main(String[] args) {
+                String s1 = "Hello";
+                System.out.println(s1.isBlank());
+        }
+
+}
