08b91609f581dc9147df3356539edab48c50c25d
KirillMar <re4r@bk.ru>
2023-03-21 20:22:50 +0300

    lesson19

--- src/main/java/lesson19/Exec19.java
+++ src/main/java/lesson19/Exec19.java
@@ -14,8 +14,16 @@ public class Exec19 {
                 System.out.println(Arrays.toString(array1));
                 
                 // Dynamic Initialization:
+                int[][] array = new int[3][];
                 
-    
+                array[0] = new int[5];
+                array[1] = new int[7];
+                array[2] = new int[9];
+                
+                for (int i = 0; i < array.length; i++) {
+                        for (int j = 0; j < array[i].length; j++) {
+                                array[i][j] = j;
+                        }
+                }
         }
-
 }
