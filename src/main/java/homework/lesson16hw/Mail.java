85408460bd76ed383a45ff80e292ad4df723d053
KirillMar <re4r@bk.ru>
2023-03-21 14:19:23 +0300

    lesson16_homework

--- src/main/java/homework/lesson16hw/Mail.java
+++ src/main/java/homework/lesson16hw/Mail.java
@@ -2,5 +2,18 @@
 package homework.lesson16hw;
 
 public class Mail {
+        
+        private static String m1 = "ya@yahoo.com";
+        private static String m2 = "one@mail.ru";
+        private static String m3 = "two@gmail.com";
+        
+        public static void main(String[] args) {
+                showDomainName(m3);
+        }
+        
+        static void showDomainName(String string) {
+                String result = string.substring(string.indexOf("@") + 1, string.indexOf("."));
+                System.out.println(result);
+        }
 
 }
