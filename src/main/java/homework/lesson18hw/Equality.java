bd0d9758ae7e058b672f801d3b4d20750f3ffc3b
KirillMar <re4r@bk.ru>
2023-03-21 16:03:24 +0300

    lesson18_homework

--- src/main/java/homework/lesson18hw/Equality.java
+++ src/main/java/homework/lesson18hw/Equality.java
@@ -2,5 +2,18 @@
 package homework.lesson18hw;
 
 public class Equality {
+        
+        private static StringBuilder s1 = new StringBuilder("HelloRave");
+        private static StringBuilder s2 = new StringBuilder("HelloRave");
+        
+        public static void main(String[] args) {
+                System.out.println(compare(s1, s2));
+        }
+        
+        static boolean compare(StringBuilder sb1, StringBuilder sb2) {
+                String str1 = new String(sb1);
+                String str2 = new String(sb2);
+                return str1.equals(str2);
+        }
 
 }
