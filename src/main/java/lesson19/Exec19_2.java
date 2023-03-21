543d882ad25e339ca96462f23f41bb84b44964bc
KirillMar <re4r@bk.ru>
2023-03-21 20:54:30 +0300

    lesson19

--- src/main/java/lesson19/Exec19_2.java
+++ src/main/java/lesson19/Exec19_2.java
@@ -1,9 +1,22 @@
 
 package lesson19;
 
+import java.util.Arrays;
+
 public class Exec19_2 {
         
         public static void main(String[] args) {
+                
+                int[] array = new int[10];
+                
+                for (int i = 0; i < array.length; i++) {
+                        array[i] = (int) (Math.random() * 10);
+                }
+                System.out.println(Arrays.toString(array));
+                
+                Arrays.sort(array);
+                
+                System.out.println(Arrays.toString(array));
                 
         }
 
