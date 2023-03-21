e00a44100921a10bbe81a7fc58796d293494e0a4
KirillMar <re4r@bk.ru>
2023-03-21 19:48:26 +0300

    lesson19

--- src/main/java/lesson19/Exec19.java
+++ src/main/java/lesson19/Exec19.java
@@ -1,9 +1,17 @@
 
 package lesson19;
 
+import java.util.Arrays;
+
 public class Exec19 {
-        
+     
         public static void main(String[] args) {
+                
+                int [] array1 = new int[10];
+                String [] array2 = new String [10];
+                double [] array3 = new double[10];
+                
+                System.out.println(Arrays.toString(array3));
                 
         }
 
