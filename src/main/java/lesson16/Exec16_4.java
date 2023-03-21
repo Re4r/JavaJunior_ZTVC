d7b55033a0a45a7f6c3ef60aa74146fd4a589b42
KirillMar <re4r@bk.ru>
2023-03-21 13:09:16 +0300

    lesson16

--- src/main/java/lesson16/Exec16_4.java
+++ src/main/java/lesson16/Exec16_4.java
@@ -3,10 +3,7 @@ package lesson16;
 
 public class Exec16_4 {
         
-        public static void main(String[] args) {
-                
-                String string;
-                string = "Соображения высшего порядка, "
+        private static final String STR = "Соображения высшего порядка, "
                                 + "а также реализация намеченного плана развития "
                                 + "представляет собой интересный эксперимент проверки "
                                 + "всесторонне сбалансированных нововведений? "
@@ -16,6 +13,29 @@ public class Exec16_4 {
                                 + "сложившаяся структура организации "
                                 + "напрямую зависит от системы обучения кадров,"
                                 + "соответствующей насущным потребностям.";
+ 
+        public static void main(String[] args) {
+                
+                transformPoetry(STR);
+                
+        }
+        
+        static void transformPoetry(String string) {
+                String string2 = string.toUpperCase();
+                char c1;
+                char c2;
+                
+                for (int i = 0; i < string.length(); i++) {
+                        c1 = string.charAt(i);
+                        c2 = string2.charAt(i);
+                        if (c1 != ',' && c1 != ':' && c1 != ' ' && c1 != '.' && c1 == c2 && c1 != '?' && c1 != '!') {
+                                System.out.println();
+                                System.out.print(c1);
+                                continue;
+                        }
+                        System.out.print(c1);
+                }
+                System.out.println();
         }
 
 }
