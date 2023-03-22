53037428a820c0fedb958f038117a43687ab9e56
KirillMar <re4r@bk.ru>
2023-03-22 13:54:52 +0300

    lesson19_homework

--- src/main/java/homework/lesson19hw/Exec19Hw.java
+++ src/main/java/homework/lesson19hw/Exec19Hw.java
@@ -1,10 +1,33 @@
 
 package homework.lesson19hw;
 
+import java.util.Arrays;
+
 public class Exec19Hw {
+        
+        private static int[] arraySource = new int[10];
         
         public static void main(String[] args) {
                 
+                for (int i = 0; i < arraySource.length; i++) {
+                        arraySource[i] = (int) (Math.random() * 10);
+                }
+                
+                System.out.println(Arrays.toString(arraySource));
+                System.out.println(Arrays.toString(sortArray(arraySource)));
+                
+        }
+        
+        static int[] sortArray(int[] array) {
+                int[] array2 = array.clone();
+                int min = array2[0];
+                for (int i = 0; i < array2.length; i++) {
+                        for (int j = 0; j < 10; j++) {
+                                
+                        }
+                            
+                }
+                return arraySource;
         }
 
 }
