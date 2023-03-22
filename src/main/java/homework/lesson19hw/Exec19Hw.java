5db7eba098d8bad90895807027301f347ed2b742
KirillMar <re4r@bk.ru>
2023-03-22 14:46:38 +0300

    lesson19_homework

--- src/main/java/homework/lesson19hw/Exec19Hw.java
+++ src/main/java/homework/lesson19hw/Exec19Hw.java
@@ -14,20 +14,23 @@ public class Exec19Hw {
                 }
                 
                 System.out.println(Arrays.toString(arraySource));
-                System.out.println(Arrays.toString(sortArray(arraySource)));
+                System.out.println(Arrays.toString(bubbleSortArray(arraySource)));
                 
         }
         
-        static int[] sortArray(int[] array) {
-                int[] array2 = new int[array.length];
-                int min = array[0];
+ 
+        static int[] bubbleSortArray(int[] array) {
                 for (int i = 0; i < array.length; i++) {
-                        if (array[i] < min) {
-                                min = array[i];
-                                array2[i] = min;
+                        for (int j = i + 1; j < array.length; j++) {
+                                int temp = 0;
+                                if (array[i] > array[j]) {
+                                        temp = array[i];
+                                        array[i] = array[j];
+                                        array[j] = temp;
+                                }
                         }
                 }
-                return array2;
+                return array;
         }
 
 }
