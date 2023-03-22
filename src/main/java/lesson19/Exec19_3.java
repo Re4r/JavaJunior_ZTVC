a96b0e41e963194d9c82815d3b5cf5b6c1be6d1a
KirillMar <re4r@bk.ru>
2023-03-22 13:32:52 +0300

    lesson19

--- src/main/java/lesson19/Exec19_3.java
+++ src/main/java/lesson19/Exec19_3.java
@@ -1,6 +1,8 @@
 
 package lesson19;
 
+import java.util.Arrays;
+
 public class Exec19_3 {
         
         private static int[] array = new int[10];
@@ -10,11 +12,32 @@ public class Exec19_3 {
                 for (int i = 0; i < array.length; i++) {
                         array[i] = (int) (Math.random() * 10);
                 }
-                
+
+                showMinMaxValue(array);
+                showMinMaxValue2(array);
         }
         
         static void showMinMaxValue(int[] array) {
-                
+                int max = array[0];
+                int min = array[0];
+                for (int i = 0; i < array.length; i++) {
+                        if (array[i] > max) {
+                                max = array[i];
+                        }
+                        if (array[i] < min) {
+                                min = array[i];
+                        }
+                }
+                System.out.println("Source Array: " + Arrays.toString(array));
+                System.out.println("Max Value: " + max);
+                System.out.println("Min Value: " + min);
+        }
+        
+        static void showMinMaxValue2(int[] array) {
+                Arrays.sort(array);
+                System.out.println("Source Array: " + Arrays.toString(array));
+                System.out.println("Max Value: " + array[array.length - 1]);
+                System.out.println("Min Value: " + array[0]);
         }
 
 }
