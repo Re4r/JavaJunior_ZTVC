cc5916ee14347e485becc688dcffaa1440837fd5
KirillMar <re4r@bk.ru>
2023-03-23 20:24:28 +0300

    lesson20

--- src/main/java/homework/lesson20hw/Exec20.java
+++ src/main/java/homework/lesson20hw/Exec20.java
@@ -1,6 +1,33 @@
 
 package homework.lesson20hw;
 
+import java.util.Arrays;
+
 public class Exec20 {
+        
+        private static final String[] ARRAY1 = new String[10];
+        private static final String[] ARRAY2 = new String[5];
+        private static final String[] ARRAY3 = new String[20];
+        
+        public static void main(String[] args) {
+                
+                initialize(ARRAY1, ARRAY2, ARRAY3);
+ 
+        }
+        
+        static String[] example(String[] ... s) {
+                
+                
+                
+        }
+        
+        static void initialize(String[] ... array) {
+                for (String[] s : array) {
+                        for (int i = 0; i < s.length; i++) {
+                                s[i] = Integer.toString((int) (Math.random() * 10));
+                        }
+                        System.out.println(Arrays.toString(s));
+                }
+        }
 
 }
