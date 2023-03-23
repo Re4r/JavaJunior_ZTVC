b98579a66e6099a8df6657d1565296294bb246c8
KirillMar <re4r@bk.ru>
2023-03-23 12:45:37 +0300

    lesson19_homework

--- src/main/java/lesson20/Exec20.java
+++ src/main/java/lesson20/Exec20.java
@@ -1,11 +1,36 @@
 
 package lesson20;
 
+import java.util.Arrays;
+
 public class Exec20 {
         
+        private static final int[][] sourceArray = new int[3][10];
+        
         public static void main(String[] args) {
-                System.out.println(args[0]);
-                System.out.println(args.length);
+                
+                initialization(sourceArray);
+                //show(sourceArray);
+                System.out.println(Arrays.toString(sourceArray[0]));
+                
         }
+        
+        static void initialization(int[][] array) {
+                for (int[] arr : array) {
+                        for (int item : arr) {
+                                item = (int) (Math.random() * 10);
+                                System.out.println(item);
+                        }
+                        System.out.println(Arrays.toString(arr));
+                }
+        }
+        
+        static void show(int[][] array) {
+                for (int[] arr : array) {
+                        System.out.println(Arrays.toString(arr));
+                }
+        }
+        
+        
 
 }
