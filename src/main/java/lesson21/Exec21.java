a2784c435c5a81b026bd1363c31da333913c67b0
KirillMar <re4r@bk.ru>
2023-03-24 20:30:00 +0300

    lesson21

---
+++ src/main/java/lesson21/Exec21.java
@@ -0,0 +1,13 @@
+
+package lesson21;
+
+import java.util.ArrayList;
+
+public class Exec21 {
+        
+        public static void main(String[] args) {
+                
+                
+        }
+
+}
