1e5dc1d6575f0834de58375985500bdeaa93a0fa
KirillMar <re4r@bk.ru>
2023-03-24 21:02:05 +0300

    lesson21

--- src/main/java/lesson21/Exec21.java
+++ src/main/java/lesson21/Exec21.java
@@ -8,13 +8,22 @@ public class Exec21 {
         public static void main(String[] args) {
                 
                 ArrayList<Integer> list = new ArrayList<>();
+                ArrayList<Integer> list2 = new ArrayList<>();
                 
-                for (int i = 0; i < 20; i++) {
+                for (int i = 0; i < 10; i++) {
                         list.add(i);
                 }
                 
+                list.add(list.size(), 999);
+                list.add(0, 111);
+                
+                for (int i = 0; i < list.size(); i++) {
+                        list2.add(list.get(i));
+                }
+                
                 System.out.println(list);
-                System.out.println(list.size());
+                System.out.println(list2);
+                
                 
         }
 
