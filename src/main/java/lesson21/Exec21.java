6a738a5fb860b748849f64e11a2c5a8b0b153067
KirillMar <re4r@bk.ru>
2023-03-24 20:36:39 +0300

    lesson21

--- src/main/java/lesson21/Exec21.java
+++ src/main/java/lesson21/Exec21.java
@@ -7,7 +7,13 @@ public class Exec21 {
         
         public static void main(String[] args) {
                 
+                ArrayList<Integer> list = new ArrayList<>();
                 
+                for (int i = 0; i < 10; i++) {
+                        list.add(i);
+                }
+                
+                System.out.println(list);
         }
 
 }
