f63bbdd3040960ff468cb233eb7a0fed3b3a7370
KirillMar <re4r@bk.ru>
2023-03-26 00:50:08 +0300

    lesson21

--- src/main/java/lesson21/Exec21_2.java
+++ src/main/java/lesson21/Exec21_2.java
@@ -1,6 +1,27 @@
 
 package lesson21;
 
+import java.util.ArrayList;
+
 public class Exec21_2 {
+        
+        public static void main(String[] args) {
+                
+                StringBuilder sb = new StringBuilder("A");
+                StringBuilder sb2 = new StringBuilder("B");
+                StringBuilder sb3 = new StringBuilder("C");
+                           
+                ArrayList<StringBuilder> list = new ArrayList<>();
+                
+                list.add(sb);
+                list.add(sb2);
+                list.add(sb3);
+                
+                System.out.println(list);
+                
+                
+                
+                
+        }
 
 }
