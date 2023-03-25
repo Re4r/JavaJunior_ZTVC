579d8d3c57ddcd1cd9497c12ff0923cb82cac59a
KirillMar <re4r@bk.ru>
2023-03-26 01:09:26 +0300

    lesson21

--- src/main/java/lesson21/Exec21_2.java
+++ src/main/java/lesson21/Exec21_2.java
@@ -2,12 +2,13 @@
 package lesson21;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Exec21_2 {
         
         public static void main(String[] args) {
                 
-                StringBuilder sb = new StringBuilder("A");
+                StringBuilder sb = new StringBuilder("AA");
                 StringBuilder sb2 = new StringBuilder("B");
                 StringBuilder sb3 = new StringBuilder("C");
                            
@@ -17,7 +18,23 @@ public class Exec21_2 {
                 list.add(sb2);
                 list.add(sb3);
                 
+                ArrayList<StringBuilder> list2 = (ArrayList<StringBuilder>) list.clone();
+                
                 System.out.println(list);
+                System.out.println(list2);
+                System.out.println(list == list2);
+                System.out.println(list.get(0) == list2.get(0));
+                
+                Object[] obj = list.toArray();
+                StringBuilder[] sba = list2.toArray(new StringBuilder[3]);
+                
+                for (Object o : obj) {
+                        System.out.println(o);
+                }
+                
+                for (StringBuilder s : sba) {
+                        System.out.println(s);
+                }
                 
                 
                 
