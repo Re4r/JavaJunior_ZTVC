6871670796ad0d8618b1864e5e68bc93eb7416cd
KirillMar <re4r@bk.ru>
2023-03-27 18:45:16 +0300

    lesson21

--- src/main/java/homework/lesson21hw/Exec21Hw.java
+++ src/main/java/homework/lesson21hw/Exec21Hw.java
@@ -1,8 +1,52 @@
 
 package homework.lesson21hw;
 
-
+import java.util.*;
+import lesson7.A;
 
 public class Exec21Hw {
+        
+        public static void main(String[] args) {
+                
+                showResult("ABC", "CDE", "RTE", "UTR");
+  
+        }
+        
+        static void showResult(String ... string) {
+                
+                ArrayList<String> list = new ArrayList<>();
+
+                for (String item : string) {
+                        list.add(item);
+                }
+                
+                System.out.println(list);
+                
+                for (int i = 0; i < list.size(); i++) {
+                        for (int j = 1; j < list.size(); j++) {
+                                if (list.get(i).equals(list.get(j))) {
+                                        list.remove(j);     
+                                }
+                        }
+                }
+                Collections.sort(list);
+                System.out.println(list);    
+        }
+        
+        static String stringGenerator() {              
+               StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < 5; i++) {
+                        sb.append(i * ((int)(Math.random() * 10)));       
+                }
+                return new String(sb); 
+        }
+        
+        static String[] stringArrayGenerator() {
+                String[] str = new String[5];
+                for (int i = 0; i < 5; i++) {
+                        str[i] = stringGenerator();
+                }
+                return str;
+        }
 
 }
