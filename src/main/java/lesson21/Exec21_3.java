5a705b91f4900552a88b961a727c6d1085e0390d
KirillMar <re4r@bk.ru>
2023-03-27 16:59:41 +0300

    lesson21

--- src/main/java/lesson21/Exec21_3.java
+++ src/main/java/lesson21/Exec21_3.java
@@ -1,6 +1,22 @@
 
 package lesson21;
 
+import java.util.*;
+
 public class Exec21_3 {
+        
+        public static void main(String[] args) {
+                
+                ArrayList<Integer> list = new ArrayList<>();
+                
+                for (int i = 0; i < 10; i++) {
+                        list.add(i * ((int)(Math.random() * 10)));
+                }
+                
+                Iterator<Integer> iterator = list.iterator();
+                while (iterator.hasNext()) {
+                        System.out.print(iterator.next() + " ");
+                }
+        }
 
 }
