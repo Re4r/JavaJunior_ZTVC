1f2f732278e012a8c6ffec7134cd2d2aea54cade
KirillMar <re4r@bk.ru>
2023-03-27 19:40:11 +0300

    lesson22

--- src/main/java/lesson22/Exec22.java
+++ src/main/java/lesson22/Exec22.java
@@ -1,10 +1,36 @@
 
 package lesson22;
 
+import java.util.Arrays;
+
 public class Exec22 {
         
         public static void main(String[] args) {
                 
+                int[] a = new int[5];
+                int[] b = new int[7];
+                int[] c = new int[9];
+                
+                initializeOfArray(a);
+                initializeOfArray(b);
+                initializeOfArray(c);
+                
+                System.out.println(Arrays.toString(a));
+                System.out.println(Arrays.toString(b));
+                System.out.println(Arrays.toString(c));
+                
+                System.out.println(Arrays.compare(a, b));
+                System.out.println(Arrays.compare(b, c));
+                System.out.println(Arrays.compare(c, a));
+
         }
+        
+        static int[] initializeOfArray(int[] array) {
+                for (int i = 0; i < array.length; i++) {
+                        array[i] = i * 10;
+                }
+                return array;
+        }
+                
 
 }
