8e0b2f161e71883c31ecc0ea3eef102bbed96dfb
KirillMar <re4r@bk.ru>
2023-03-29 15:57:47 +0300

    lesson25_homework

--- src/main/java/homework/lesson25hw/Exec25Hw.java
+++ src/main/java/homework/lesson25hw/Exec25Hw.java
@@ -4,7 +4,23 @@ package homework.lesson25hw;
 public class Exec25Hw {
     
     public static void main(String[] args) {
+        Student student = new Student(new StringBuilder("Joseph"), 4, 5);
+        student.showInfo();
+        showStudentInfo(student);
+        student.getName().append("QQQQ");
+        student.showInfo();
+        showStudentInfo(student);
+        student.setName(new StringBuilder("Jannet"));
+        student.setCourse(5);
+        student.setGrade(4);
+        showStudentInfo(student);
         
+    }
+    
+    private static void showStudentInfo(Student student) {
+        System.out.println(student.getName());
+        System.out.println(student.getCourse());
+        System.out.println(student.getGrade());
     }
 
 }
--- src/main/java/homework/lesson25hw/Student.java
+++ src/main/java/homework/lesson25hw/Student.java
@@ -2,6 +2,46 @@ package homework.lesson25hw;
 
 public class Student {
     
+    private StringBuilder name;
+    private int course;
+    private int grade;
+
+    public Student(StringBuilder name, int course, int grade) {
+        this.name = name;
+        this.course = course;
+        this.grade = grade;
+    }
+
+    public StringBuilder getName() {
+        StringBuilder sb = new StringBuilder(this.name);
+        return sb;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setName(StringBuilder name) {
+        this.name = name;
+    }
+
+    public void setCourse(int course) {
+        this.course = course;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
     
+    public void showInfo() {
+        System.out.println(this.name);
+        System.out.println(this.course);
+        System.out.println(this.grade);
+    }
+
 
 }
