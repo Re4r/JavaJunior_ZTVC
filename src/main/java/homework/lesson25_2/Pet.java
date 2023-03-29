172fe49622ebbad30a6a5aecbf0d5cce9051db16
KirillMar <re4r@bk.ru>
2023-03-29 16:11:23 +0300

    lesson25_homework

--- src/main/java/homework/lesson25_2/Pet.java
+++ src/main/java/homework/lesson25_2/Pet.java
@@ -1,10 +1,27 @@
 package homework.lesson25_2;
 
-public class Pet extends Animal{
+public class Pet extends Animal {
 
-    public Pet(int eyesCount) {
+    private String name;
+    private boolean tail;
+    private int pawCount;
+
+    public Pet(String name, boolean tail, int pawCount, int eyesCount) {
         super(eyesCount);
+        this.name = name;
+        this.tail = tail;
+        this.pawCount = pawCount;
         System.out.println("I am a Pet");
     }
+    
+    public void run() {
+        System.out.println("Pet Runs");
+    }
+    
+    public void jump() {
+        System.out.println("Pet Jumps");
+    }
+    
+    
 
 }
