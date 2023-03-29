e2849ff9bdc945b8e59fdf4a031c735c730f2f7c
KirillMar <re4r@bk.ru>
2023-03-29 16:20:25 +0300

    lesson25_homework

--- src/main/java/homework/lesson25_2/Exec25.java
+++ src/main/java/homework/lesson25_2/Exec25.java
@@ -2,7 +2,14 @@ package homework.lesson25_2;
 
 public class Exec25 {
     public static void main(String[] args) {
+        Dog dog = new Dog("Tiny", true, 4, 2);
+        Cat cat = new Cat("Boon", true, 4, 2);
         
+        dog.eat();
+        dog.drink();
+        dog.run();
+        dog.jump();
+        dog.play();
     }
 
 }
