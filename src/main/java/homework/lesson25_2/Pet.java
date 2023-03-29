13b3c047322a55e65f8f7f0b06968e9fec94c48f
KirillMar <re4r@bk.ru>
2023-03-29 16:05:33 +0300

    lesson25_homework

---
+++ src/main/java/homework/lesson25_2/Pet.java
@@ -0,0 +1,10 @@
+package homework.lesson25_2;
+
+public class Pet extends Animal{
+
+    public Pet(int eyesCount) {
+        super(eyesCount);
+        System.out.println("I am a Pet");
+    }
+
+}
