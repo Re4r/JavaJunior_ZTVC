e01192b90743e4fb732e84e884aba225383421db
KirillMar <re4r@bk.ru>
2023-03-29 16:03:53 +0300

    lesson25_homework

--- src/main/java/homework/lesson25_2/Animal.java
+++ src/main/java/homework/lesson25_2/Animal.java
@@ -2,4 +2,19 @@ package homework.lesson25_2;
 
 public class Animal {
 
+    private int eyesCount;
+
+    public Animal(int eyesCount) {
+        this.eyesCount = eyesCount;
+        System.out.println("I am an Animal");
+    }
+
+    public void eat() {
+        System.out.println("Animal Eat");
+    }
+
+    public void drink() {
+        System.out.println("Animal Drink");
+    }
+
 }
