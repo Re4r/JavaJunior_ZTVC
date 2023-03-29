a061dde4e0f54f559e77ade393dac2fd955e2a31
KirillMar <re4r@bk.ru>
2023-03-29 14:33:40 +0300

    lesson25

--- src/main/java/lesson25/Exec25.java
+++ src/main/java/lesson25/Exec25.java
@@ -1,16 +1,41 @@
 package lesson25;
 
 public class Exec25 {
-    
+
     public static void main(String[] args) {
-        Citizen citizen = new Citizen("Male",new StringBuilder("Alex"), 25, 75);
+        Citizen citizen = new Citizen("Male", new StringBuilder("Alex"), 25, 75);
         citizen.setAge(24);
         citizen.setName(new StringBuilder("Philiz"));
         citizen.setWeight(80);
-        
+
         citizen.getName().append("!!!");
         System.out.println(citizen.getAge());
         System.out.println(citizen.getName());
+        
+        
+
+    }
+
+    class Postman extends Citizen {
+
+        public Postman(String sex, StringBuilder name, int age, int weight) {
+            super(sex, name, age, weight);
+        }
+
+        public void mailDelivery() {
+            System.out.println("Mail delivered");
+        }
+    }
+    
+    class Spaceman extends Citizen {
+        
+        public Spaceman(String sex, StringBuilder name, int age, int weight) {
+            super(sex, name, age, weight);
+        }
+        
+        public void spaceTraveling() {
+            System.out.println("Welcome to moon");
+        }
         
     }
 
