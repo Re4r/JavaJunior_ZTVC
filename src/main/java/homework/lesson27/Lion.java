e9b169b84f6f1523d54b70030286b2a011f1ad55
KirillMar <re4r@bk.ru>
2023-04-01 00:55:37 +0300

    lesson27_homework

--- src/main/java/homework/lesson27/Lion.java
+++ src/main/java/homework/lesson27/Lion.java
@@ -1,5 +1,32 @@
 package homework.lesson27;
 
-public class Lion {
+public class Lion extends Mammal{
+    
+    private String name;
+
+    public Lion(String name) {
+        super(name);
+        this.name = name;
+    }
+
+    @Override
+    void run() {
+        System.out.println("Lion Runs");
+    }
+
+    @Override
+    void eat() {
+        System.out.println("Lion Eats");
+    }
+
+    @Override
+    void sleep() {
+        System.out.println("Lion Sleep");
+    }
+    
+    
+    
+    
+    
 
 }
