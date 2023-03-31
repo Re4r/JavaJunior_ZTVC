29a8ea6acf32b5aa6b8028fb3453a1b5aa8c5644
KirillMar <re4r@bk.ru>
2023-03-31 12:50:42 +0300

    lesson27_homework

--- src/main/java/homework/lesson27/Animal.java
+++ src/main/java/homework/lesson27/Animal.java
@@ -2,9 +2,14 @@ package homework.lesson27;
 
 abstract class Animal {
 
+    private String name;
+
     public Animal(String name) {
+        this.name = name;
     }
-    
-    
+
+    abstract void eat();
+
+    abstract void sleep();
 
 }
---
+++ src/main/java/homework/lesson27/Fish.java
@@ -0,0 +1,16 @@
+package homework.lesson27;
+
+abstract class Fish extends Animal{
+    
+    private String name;
+    
+    public Fish(String name) {
+        super(name);
+        this.name = name;
+    }
+    
+    
+    
+    
+
+}
