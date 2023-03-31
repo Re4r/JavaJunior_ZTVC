f85e6d025e9d26f357872b63f33e26911c8cfdf3
KirillMar <re4r@bk.ru>
2023-04-01 00:46:19 +0300

    lesson27_homework

--- src/main/java/homework/lesson27/Pinguin.java
+++ src/main/java/homework/lesson27/Pinguin.java
@@ -1,5 +1,32 @@
 package homework.lesson27;
 
-public class Pinguin {
+public class Pinguin extends Bird implements Speakable {
+
+    private String name;
+
+    public Pinguin(String name) {
+        super(name);
+        this.name = name;
+    }
+
+    @Override
+    void fly() {
+
+    }
+
+    @Override
+    void eat() {
+
+    }
+
+    @Override
+    void sleep() {
+
+    }
+
+    @Override
+    public void speak() {
+        super.speak();
+    }
 
 }
