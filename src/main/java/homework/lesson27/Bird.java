f9174794db63a53066442a468153ed45e15d97d8
KirillMar <re4r@bk.ru>
2023-03-31 12:56:13 +0300

    lesson27_homework

---
+++ src/main/java/homework/lesson27/Bird.java
@@ -0,0 +1,13 @@
+package homework.lesson27;
+
+abstract class Bird extends Animal{
+    
+    private String name;
+
+    public Bird(String name) {
+        super(name);
+        this.name = name;
+    }
+    
+
+}
--- src/main/java/homework/lesson27/Fish.java
+++ src/main/java/homework/lesson27/Fish.java
@@ -1,16 +1,19 @@
 package homework.lesson27;
 
-abstract class Fish extends Animal{
-    
+abstract class Fish extends Animal {
+
     private String name;
-    
+
     public Fish(String name) {
         super(name);
         this.name = name;
     }
+
+    @Override
+    void sleep() {
+        System.out.println("Fish Sleep");
+    }
     
-    
-    
-    
+    abstract void swim();
 
 }
