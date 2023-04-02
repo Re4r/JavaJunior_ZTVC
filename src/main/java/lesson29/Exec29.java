9d4f7917ce505b4d9b73833e9ac94c4fb2a30447
KirillMar <re4r@bk.ru>
2023-04-02 15:52:44 +0300

    lesson29

--- src/main/java/lesson29/Exec29.java
+++ src/main/java/lesson29/Exec29.java
@@ -1,13 +1,52 @@
 package lesson29;
 
 public class Exec29 {
-    
+
     public static void main(String[] args) {
-        
+
     }
 
 }
 
 abstract class Employee {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    void sleep() {
+        System.out.println(name + " sleeps");
+    }
+
+    abstract void work();
+
+}
+
+interface Helpable {
+
+    void help();
+}
+
+class Driver extends Employee {
+
+    public Driver(String name) {
+        super(name);
+    }
+
+    @Override
+    void work() {
+        System.out.println("Driver " + this.getName() + " works");
+    }
+
+    @Override
+    void sleep() {
+        super.sleep();
+    }
 
 }
