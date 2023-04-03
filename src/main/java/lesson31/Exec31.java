519f7cf5ef08f0d8c84ef6bb7620d8cfe05768c4
KirillMar <re4r@bk.ru>
2023-04-03 20:12:20 +0300

    lesson31

--- src/main/java/lesson31/Exec31.java
+++ src/main/java/lesson31/Exec31.java
@@ -1,14 +1,18 @@
 package lesson31;
+
 import java.io.*;
+
 public class Exec31 {
-    
+
     public static void main(String[] args) throws Exception {
         File file = new File("D:\\lessonZT\\test31.txt");
-        FileInputStream fis = new FileInputStream(file);
-        fis.read();
         FileOutputStream fos = new FileOutputStream(file);
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 10; i++) {
             fos.write(i);
+        }
+        FileInputStream fis = new FileInputStream(file);
+        for (int i = 0; i < 10; i++) {
+            System.out.print(" " + fis.read());
         }
     }
 
