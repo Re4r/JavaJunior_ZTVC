be35681cbb384dedea21c74248dd9e20f04c4413
KirillMar <re4r@bk.ru>
2023-04-03 20:09:11 +0300

    lesson31

--- src/main/java/lesson31/Exec31.java
+++ src/main/java/lesson31/Exec31.java
@@ -2,8 +2,14 @@ package lesson31;
 import java.io.*;
 public class Exec31 {
     
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws Exception {
+        File file = new File("D:\\lessonZT\\test31.txt");
+        FileInputStream fis = new FileInputStream(file);
+        fis.read();
+        FileOutputStream fos = new FileOutputStream(file);
+        for (int i = 0; i < 100; i++) {
+            fos.write(i);
+        }
     }
 
 }
