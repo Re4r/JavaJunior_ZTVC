5d4d838a3081077176e0a1bf2155e6d6430a8406
KirillMar <re4r@bk.ru>
2023-04-03 15:23:10 +0300

    lesson30

--- src/main/java/lesson30/Exec302.java
+++ src/main/java/lesson30/Exec302.java
@@ -1,10 +1,30 @@
 package lesson30;
 
 public class Exec302 {
+
+    private String name;
+
+    public Exec302() {
+        System.out.println("Empty Constructor");
+    }
+
+    public Exec302(String name) {
+        this();
+        this.name = name;
+        System.out.println("Hello " + name + " from Constructor");
+    }
+
+    {
+        System.out.println("Hello " + name + " from Initializer Block");
+    }
     
+    static {
+        System.out.println("Hello from Static Initializer Block");
+    }
+
     public static void main(String[] args) {
-         
-        
+
+        Exec302 ex = new Exec302("Qwerty");
     }
 
 }
