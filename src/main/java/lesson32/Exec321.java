8413b354b1130f091dd56b47c9bda9c64a9206b3
KirillMar <re4r@bk.ru>
2023-04-04 20:49:03 +0300

    lesson321

--- src/main/java/lesson32/Exec321.java
+++ src/main/java/lesson32/Exec321.java
@@ -1,11 +1,44 @@
 package lesson32;
 
-public class Exec321 {
-    
-    public static void main(String[] args) {
-        
-    }
-    
-    
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
+public class Exec321 {
+
+    public static void main(String[] args) {
+
+        File file1 = new File("D:\\lessonZT\\source.txt");
+        File file2 = new File("D:\\lessonZT\\dest.txt");
+        
+        copyFile(file1, file2);
+
+    }
+
+    static void copyFile(File file1, File file2) {
+        FileInputStream fis = null;
+        FileOutputStream fos = null;
+
+        try {
+            fis = new FileInputStream(file1);
+            fos = new FileOutputStream(file2);
+            fos.write(fis.readAllBytes());
+        } catch (FileNotFoundException fnfe) {
+            fnfe.getMessage();
+            fnfe.printStackTrace();
+        } catch (IOException ioe) {
+            ioe.getMessage();
+            ioe.printStackTrace();
+        } finally {
+            try {
+                fis.close();
+                fos.close();
+            } catch (IOException ioe) {
+                ioe.getMessage();
+                ioe.printStackTrace();
+            }
+        }
+    }
 }
