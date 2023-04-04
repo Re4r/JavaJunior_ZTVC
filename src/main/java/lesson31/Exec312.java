c80ab6e9196b7d1925e32cb8663f7e4da96ea4c3
KirillMar <re4r@bk.ru>
2023-04-04 13:51:53 +0300

    lesson312

--- src/main/java/lesson31/Exec312.java
+++ src/main/java/lesson31/Exec312.java
@@ -1,5 +1,29 @@
 package lesson31;
 
-public class Exec312 {
+public final class Exec312 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
+final class Test {
+
+    private int[] createArray(final int length) {
+        int[] array = new int[length];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+        }
+        return array;
+    }
+
+    void showArrayElement(final int[] array, final int index) {
+        try {
+            System.out.println(array[index]);
+        } catch (ArrayIndexOutOfBoundsException aioobe) {
+            System.out.println(aioobe);
+        }
+    }
 
 }
