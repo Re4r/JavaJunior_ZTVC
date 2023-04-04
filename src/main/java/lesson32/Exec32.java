d1557e76c4d6bca9b315ce0293ef5e87db768914
KirillMar <re4r@bk.ru>
2023-04-04 19:45:46 +0300

    lesson32

--- src/main/java/lesson32/Exec32.java
+++ src/main/java/lesson32/Exec32.java
@@ -2,4 +2,34 @@ package lesson32;
 
 public class Exec32 {
 
+    public static void main(String[] args) {
+
+    }
+
+}
+
+class NotMeat extends RuntimeException {
+
+    public NotMeat(String message) {
+        super(message);
+    }
+
+}
+
+class NotWater extends Exception {
+
+    public NotWater(String message) {
+        super(message);
+    }
+
+}
+
+class Tiger {
+    void eat(String item) {
+        if (!item.equals("meat")) {
+            throw new NotMeat("Tiger not eat " + item);
+        } else {
+            System.out.println("Tiger eats " + item);
+        }
+    }
 }
