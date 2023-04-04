7d931001ca508a2b51a75ea7124e21586479648d
KirillMar <re4r@bk.ru>
2023-04-04 19:56:15 +0300

    lesson32

--- src/main/java/lesson32/Exec32.java
+++ src/main/java/lesson32/Exec32.java
@@ -3,6 +3,21 @@ package lesson32;
 public class Exec32 {
 
     public static void main(String[] args) {
+        
+        Tiger tiger = new Tiger();
+        tiger.eat("meat");
+        try {
+            tiger.drink("water");
+            try {
+                tiger.drink("beer");
+            } catch (NotWater nw) {
+                nw.getMessage();
+            }
+        } catch (Exception e) {
+            e.getMessage();
+        } finally {
+            System.out.println("This is finally block");
+        }
 
     }
 
@@ -25,11 +40,20 @@ class NotWater extends Exception {
 }
 
 class Tiger {
+
     void eat(String item) {
         if (!item.equals("meat")) {
-            throw new NotMeat("Tiger not eat " + item);
+            throw new NotMeat("Tiger don't eat " + item);
         } else {
             System.out.println("Tiger eats " + item);
+        }
+    }
+
+    void drink(String item) throws NotWater{
+        if (!item.equals("water")) {
+            throw new NotWater("Tiger don't drink " + item);
+        } else {
+            System.out.println("Tiger drinks " + item);
         }
     }
 }
