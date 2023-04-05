b26bcfdc94ab500764f34e25d0d19f7946309d4f
KirillMar <re4r@bk.ru>
2023-04-05 21:29:13 +0300

    lesson333

--- src/main/java/lesson34/Exec34.java
+++ src/main/java/lesson34/Exec34.java
@@ -1,5 +1,35 @@
 package lesson34;
 
+import java.util.ArrayList;
+
 public class Exec34 {
+
+    public static void main(String[] args) {
+
+        Robot.factory(10);
+
+    }
+
+}
+
+class Robot {
+
+    private int weight;
+
+    public Robot(int weight) {
+        this.weight = weight;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    static void factory(int quantity) {
+        ArrayList<Robot> robots = new ArrayList<>();
+        for (int i = 1; i <= quantity; i++) {
+            robots.add(new Robot(i * ((int) (Math.random() * 100))));
+        }
+        System.out.println(robots.get(0).getWeight());
+    }
 
 }
