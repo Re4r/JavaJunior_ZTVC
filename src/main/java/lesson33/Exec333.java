ad829fe4eefef57ddb0dde2ea12a6d1fea50c852
KirillMar <re4r@bk.ru>
2023-04-05 20:28:20 +0300

    lesson333

--- src/main/java/lesson33/Exec333.java
+++ src/main/java/lesson33/Exec333.java
@@ -6,18 +6,24 @@ import java.time.format.*;
 public class Exec333 {
 
     public static void main(String[] args) {
-        LocalDate ldt1 = LocalDate.now();
-        LocalDate ldt2 = LocalDate.of(2024, Month.SEPTEMBER, 17);
-        DateTimeFormatter dtf = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT);
+        LocalDateTime ldt1 = LocalDateTime.now().now();
+        LocalDateTime ldt2 = LocalDateTime.of(2025, Month.MARCH, 10, 22, 37);
+        DateTimeFormatter dtf = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT);
         Period period = Period.ofMonths(1);
-        Duration duration = Duration.ofDays(10);
+        Duration duration = Duration.ofDays(1);
+        System.out.println(duration);
+
+        workTrace(ldt1, ldt2, period, duration);
 
     }
-    
-    static void workTrace(LocalDate ld, LocalDate ld1) {
-        LocalDate date = ld;
-        while (date.isBefore(ld1)) {
-            
+
+    static void workTrace(LocalDateTime ld, LocalDateTime ld1, Period p, Duration d) {
+        LocalDateTime date = ld;
+        LocalDateTime date2 = ld1;
+        while (date.isBefore(date2)) {
+            System.out.println("From: " + date + "To: " + date2);
+            date = date.plus(p);
+            date2 = date2.minus(d);
         }
     }
 
