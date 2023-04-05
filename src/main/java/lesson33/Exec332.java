bb3831df2b086c75d12cd35cfe18219a25d0e3ea
KirillMar <re4r@bk.ru>
2023-04-05 18:51:55 +0300

    lesson332

--- src/main/java/lesson33/Exec332.java
+++ src/main/java/lesson33/Exec332.java
@@ -1,17 +1,17 @@
 package lesson33;
 
 import java.time.*;
-import java.util.ArrayList;
+import java.time.format.*;
+
 
 public class Exec332 {
     
     public static void main(String[] args) {
         
         LocalDate ld = LocalDate.now();
-        System.out.println(ld.getDayOfWeek());
-        System.out.println(ld.getDayOfMonth());
-        System.out.println(ld.getDayOfYear());
-        System.out.println(ld.getMonthValue());
+        DateTimeFormatter dtf = DateTimeFormatter.ISO_DATE;
+        System.out.println(ld.format(dtf));
+
     }
 
 }
