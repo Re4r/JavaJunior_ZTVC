347447e8dab5634d9403384d09e2e0aa88f8b3ff
KirillMar <re4r@bk.ru>
2023-04-05 15:02:30 +0300

    lesson33

--- src/main/java/lesson33/Exec33.java
+++ src/main/java/lesson33/Exec33.java
@@ -8,6 +8,16 @@ public class Exec33 {
         System.out.println(LocalTime.now());
         System.out.println(LocalDate.now());
         System.out.println(LocalDateTime.now());
+        System.out.println("-----------------");
+        
+        LocalDate ld1 = LocalDate.of(2023, 1, 31);
+        LocalDate ld2 = LocalDate.of(2023, Month.JANUARY, 31);
+        
+        System.out.println(ld1);
+        System.out.println(ld2);
+
+
+            
     }
 
 }
