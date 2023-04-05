b35dd6c95defe52805779cbf9c41871b029b5ab1
KirillMar <re4r@bk.ru>
2023-04-05 19:31:40 +0300

    lesson332

--- src/main/java/lesson33/Exec332.java
+++ src/main/java/lesson33/Exec332.java
@@ -3,11 +3,10 @@ package lesson33;
 import java.time.*;
 import java.time.format.*;
 
-
 public class Exec332 {
-    
+
     public static void main(String[] args) {
-        
+
         LocalDate ld = LocalDate.now();
         DateTimeFormatter dtf = DateTimeFormatter.ISO_DATE;
         System.out.println(ld);
@@ -16,14 +15,14 @@ public class Exec332 {
         DateTimeFormatter dtf1 = DateTimeFormatter.ISO_TIME;
         System.out.println(lt);
         System.out.println(lt.format(dtf1));
-        LocalDateTime ldt =LocalDateTime.now();
+        LocalDateTime ldt = LocalDateTime.now();
         DateTimeFormatter dtf2 = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
         System.out.println(ldt);
         System.out.println(ldt.format(dtf2));
         System.out.println(ldt.format(dtf));
         System.out.println(ldt.format(dtf1));
-        DateTimeFormatter dtf3 =DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT);
-        DateTimeFormatter dtf4 =DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT, FormatStyle.SHORT);
+        DateTimeFormatter dtf3 = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT);
+        DateTimeFormatter dtf4 = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT, FormatStyle.SHORT);
         System.out.println(ld);
         System.out.println(ld.format(dtf3));
         System.out.println(ldt);
