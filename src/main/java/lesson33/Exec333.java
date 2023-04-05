7356555c8f347944d6301e6e150b1dd345f1837f
KirillMar <re4r@bk.ru>
2023-04-05 20:31:56 +0300

    lesson333

--- src/main/java/lesson33/Exec333.java
+++ src/main/java/lesson33/Exec333.java
@@ -4,11 +4,12 @@ import java.time.*;
 import java.time.format.*;
 
 public class Exec333 {
+    
+    static DateTimeFormatter dtf = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT);
 
     public static void main(String[] args) {
         LocalDateTime ldt1 = LocalDateTime.now().now();
         LocalDateTime ldt2 = LocalDateTime.of(2025, Month.MARCH, 10, 22, 37);
-        DateTimeFormatter dtf = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT);
         Period period = Period.ofMonths(1);
         Duration duration = Duration.ofDays(1);
         System.out.println(duration);
@@ -21,7 +22,7 @@ public class Exec333 {
         LocalDateTime date = ld;
         LocalDateTime date2 = ld1;
         while (date.isBefore(date2)) {
-            System.out.println("From: " + date + "To: " + date2);
+            System.out.println("From: " + date.format(dtf) + " To: " + date2.format(dtf));
             date = date.plus(p);
             date2 = date2.minus(d);
         }
