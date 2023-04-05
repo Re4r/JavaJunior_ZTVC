01d9b9981f2972fee34d13675a4934ffa4e370fe
KirillMar <re4r@bk.ru>
2023-04-05 15:13:31 +0300

    lesson33

--- src/main/java/lesson33/Exec33.java
+++ src/main/java/lesson33/Exec33.java
@@ -3,21 +3,25 @@ package lesson33;
 import java.time.*;
 
 public class Exec33 {
-    
+
     public static void main(String[] args) {
         System.out.println(LocalTime.now());
         System.out.println(LocalDate.now());
         System.out.println(LocalDateTime.now());
         System.out.println("-----------------");
-        
+
         LocalDate ld1 = LocalDate.of(2023, 1, 31);
         LocalDate ld2 = LocalDate.of(2023, Month.JANUARY, 31);
-        
+        LocalTime lt1 = LocalTime.of(22, 15);
+        LocalTime lt2 = LocalTime.of(22, 15, 30);
+        LocalTime lt3 = LocalTime.of(22, 15, 30, 999999999);
+
         System.out.println(ld1);
         System.out.println(ld2);
+        System.out.println(lt1);
+        System.out.println(lt2);
+        System.out.println(lt3);
 
-
-            
     }
 
 }
