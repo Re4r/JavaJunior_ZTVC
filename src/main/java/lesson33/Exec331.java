e91ad76892b6008c0af4fbc716e0bd0b2f2d7cd7
KirillMar <re4r@bk.ru>
2023-04-05 16:00:28 +0300

    lesson331

--- src/main/java/lesson33/Exec331.java
+++ src/main/java/lesson33/Exec331.java
@@ -6,12 +6,19 @@ public class Exec331 {
     
     public static void main(String[] args) {
         
+        LocalDate ld1 = LocalDate.of(2023, Month.JANUARY, 1);
+        LocalDate ld2 = LocalDate.of(2024, Month.DECEMBER, 31);
+        Period p = Period.ofMonths(1);
+        
+        changeModule(ld1, ld2, p);
+        
     }
     
     static void changeModule(LocalDate start, LocalDate finish, Period period) {
         LocalDate ld = start;
         while (ld.isBefore(finish)) {
-        
+            System.out.println("Date: " + ld + " - Module Changed");
+            ld = ld.plus(period);
         }
     }
 
