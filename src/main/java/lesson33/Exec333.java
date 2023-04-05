23caeca7372e700f330c693a34ada478e06740bb
KirillMar <re4r@bk.ru>
2023-04-05 20:15:34 +0300

    lesson333

--- src/main/java/lesson33/Exec333.java
+++ src/main/java/lesson33/Exec333.java
@@ -5,4 +5,20 @@ import java.time.format.*;
 
 public class Exec333 {
 
+    public static void main(String[] args) {
+        LocalDate ldt1 = LocalDate.now();
+        LocalDate ldt2 = LocalDate.of(2024, Month.SEPTEMBER, 17);
+        DateTimeFormatter dtf = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT);
+        Period period = Period.ofMonths(1);
+        Duration duration = Duration.ofDays(10);
+
+    }
+    
+    static void workTrace(LocalDate ld, LocalDate ld1) {
+        LocalDate date = ld;
+        while (date.isBefore(ld1)) {
+            
+        }
+    }
+
 }
