df6b00e71b3335f64f24f0b536f3adf1b9bfc434
KirillMar <re4r@bk.ru>
2023-04-05 15:51:26 +0300

    lesson331

---
+++ src/main/java/lesson33/Exec331.java
@@ -0,0 +1,18 @@
+package lesson33;
+
+import java.time.*;
+
+public class Exec331 {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+    static void changeModule(LocalDate start, LocalDate finish, Period period) {
+        LocalDate ld = start;
+        while (ld.isBefore(finish)) {
+        
+        }
+    }
+
+}
