ee012539b1bdb65132b78cfee14c617093c4c1bd
KirillMar <re4r@bk.ru>
2023-04-05 14:55:18 +0300

    lesson33

---
+++ src/main/java/lesson33/Exec33.java
@@ -0,0 +1,13 @@
+package lesson33;
+
+import java.time.*;
+
+public class Exec33 {
+    
+    public static void main(String[] args) {
+        System.out.println(LocalTime.now());
+        System.out.println(LocalDate.now());
+        System.out.println(LocalDateTime.now());
+    }
+
+}
