a5922628414d2130f67eccc257d50fb49c188616
KirillMar <re4r@bk.ru>
2023-04-05 19:20:56 +0300

    lesson332

--- src/main/java/lesson33/Exec332.java
+++ src/main/java/lesson33/Exec332.java
@@ -10,7 +10,16 @@ public class Exec332 {
         
         LocalDate ld = LocalDate.now();
         DateTimeFormatter dtf = DateTimeFormatter.ISO_DATE;
+        System.out.println(ld);
         System.out.println(ld.format(dtf));
+        LocalTime lt = LocalTime.now();
+        DateTimeFormatter dtf1 = DateTimeFormatter.ISO_TIME;
+        System.out.println(lt);
+        System.out.println(lt.format(dtf1));
+        LocalDateTime ldt =LocalDateTime.now();
+        DateTimeFormatter dtf2 = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+        System.out.println(ldt);
+        System.out.println(ldt.format(dtf2));
 
     }
 
