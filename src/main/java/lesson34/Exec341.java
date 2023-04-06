79e8c872e802ca0c6c515942423a55dcad204bbf
KirillMar <re4r@bk.ru>
2023-04-06 13:00:46 +0300

    lesson333

--- src/main/java/lesson34/Exec341.java
+++ src/main/java/lesson34/Exec341.java
@@ -1,5 +1,19 @@
 package lesson34;
 
 public class Exec341 {
+    
+    public static void main(String[] args) {
+        
+        example(x -> x.length());
+        
+    }
+    
+    static void example(Test t) {
+        System.out.println(t.testmethod("Hello World"));
+    }
 
+}
+@FunctionalInterface
+interface Test {
+    int testmethod(String s);
 }
