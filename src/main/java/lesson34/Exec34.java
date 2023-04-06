a512ec24711e306e5c6cc5f4a1c3ac14c1ca00af
KirillMar <re4r@bk.ru>
2023-04-06 12:31:13 +0300

    lesson333

--- src/main/java/lesson34/Exec34.java
+++ src/main/java/lesson34/Exec34.java
@@ -5,8 +5,12 @@ import java.util.ArrayList;
 public class Exec34 {
 
     public static void main(String[] args) {
-
-        Robot.factory(10);
+        
+        var robots = Robot.factory(10);
+        Robot.showItems(robots);
+        var filteredRobots = Robot.weightFilter(robots, ((r) -> r.getWeight() > 100));
+        
+ 
 
     }
 
@@ -24,12 +28,36 @@ class Robot {
         return weight;
     }
 
-    static void factory(final int quantity) {
+    static ArrayList<Robot> factory(final int quantity) {
         ArrayList<Robot> robots = new ArrayList<>();
         for (int i = 1; i <= quantity; i++) {
-            robots.add(new Robot(i * ((int) (Math.random() * 100))));
+            robots.add(new Robot(i * ((int) (Math.random() * 99))));
         }
-        System.out.println(robots.get(0).getWeight());
+        return robots;
+    }
+    
+    static ArrayList<Robot> weightFilter(ArrayList<Robot> alr, Filtration filtration) {
+        ArrayList<Robot> frs = new ArrayList<>();
+        for (Robot r : alr) {
+            if (filtration.filter(r)) {
+                frs.add(r);
+            }
+        }
+        return frs;
+    }
+    
+    static void showItems(ArrayList<Robot> robots) {
+        ArrayList<Integer> is = new ArrayList<>();
+        for (Robot r : robots) {
+            is.add(r.getWeight());
+        }
+        System.out.println(is.toString());
     }
 
+}
+
+@FunctionalInterface
+interface Filtration {
+
+    boolean filter(Robot robot);
 }
