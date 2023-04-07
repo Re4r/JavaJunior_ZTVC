c49fd93fa4eddb3d448a60a4fd18b4e3b66ee96b
KirillMar <re4r@bk.ru>
2023-04-07 13:45:16 +0300

    lesson34

--- src/main/java/lesson34/Exec342.java
+++ src/main/java/lesson34/Exec342.java
@@ -1,5 +1,57 @@
 package lesson34;
 
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
 public class Exec342 {
+
+    public static void main(String[] args) {
+        
+        ArrayList<Emploee> emploees = new ArrayList<>();
+        emploees.add(new Emploee("Alex", "IT", 300));
+        emploees.add(new Emploee("Susana", "Management", 150));
+        emploees.add(new Emploee("Janet", "Administration", 130));
+        emploees.add(new Emploee("Patrick", "Engeneering", 290));
+        
+        for (Emploee e : emploees) {
+            showEmployeeInformation(e);
+        }
+
+    }
+
+    static void showEmployeeInformation(Emploee e) {
+        System.out.println("Name: " + e.getName());
+        System.out.println("Department: " + e.getDepartment());
+        System.out.println("Salary: " + e.getSalary());
+        System.out.println("------------------------");
+    }
+    
+    
+
+}
+
+class Emploee {
+
+    private String name;
+    private String department;
+    private int salary;
+
+    public Emploee(String name, String department, int salary) {
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
 
 }
