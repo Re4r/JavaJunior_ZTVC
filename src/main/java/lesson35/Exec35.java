9d9768914a3b855e705b093c5c8f21f490c9a100
KirillMar <re4r@bk.ru>
2023-04-08 17:44:37 +0300

    lesson35

--- src/main/java/lesson35/Exec35.java
+++ src/main/java/lesson35/Exec35.java
@@ -1,15 +1,39 @@
 package lesson35;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 import java.util.function.Predicate;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Exec35 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException{
 
+        File file = new File("D:\\lessonZT\\names.txt");
+        
+        ArrayList<String> names = new ArrayList<>();
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
+        String line;
+        while ((line = br.readLine()) != null) {
+            names.add(line);
+        }
+        for (String s : names) {
+            System.out.println(s);
+        }
+    
     }
+    
+    
+    
+    
 
 }
 
