9b5748cb204fede15ed0eb7a53ede2efee0a37de
KirillMar <re4r@bk.ru>
2023-04-08 16:43:35 +0300

    lesson35

--- src/main/java/lesson35/Exec35.java
+++ src/main/java/lesson35/Exec35.java
@@ -7,4 +7,28 @@ import java.util.function.Predicate;
 
 public class Exec35 {
 
+    public static void main(String[] args) {
+
+    }
+
+}
+
+final class Unit {
+
+    private String name;
+    private int armor;
+
+    public Unit(String name, int armor) {
+        this.name = name;
+        this.armor = armor;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
 }
